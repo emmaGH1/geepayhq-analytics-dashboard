59c6fdb46e371398e0bd4b20290bdccaec07f195
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-24 13:36:11 +0100

    feat: Setup app page

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Dashboard } from "./components/Dashboard";
+import { Sidenav } from "./components/Sidenav";
+import { Topbar } from "./components/Topbar";
+
+
+const App = () => {
+  return (
+    <div className="bg-background text-secondary flex flex-row w-full">
+       <Sidenav />
+       <div className="flex flex-col ">
+          <Topbar />
+          <Dashboard />
+       </div>
+    </div>
+  );
+};
+
+export default App;
