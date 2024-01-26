55e8e930ed37af47879db1be4a057bae771b0860
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-26 04:46:19 +0100

    feat: Add Sales trend

--- src/components/SalesTrend.tsx
+++ src/components/SalesTrend.tsx
@@ -10,6 +10,7 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import type { ChartData, ChartOptions } from 'chart.js';
+import { ArrowDown } from '../constants';
   
 export default function SalesTrend() {
     ChartJS.register(
@@ -22,33 +23,42 @@ export default function SalesTrend() {
 ); 
     
 const salesData = [7, 22, 3, 23, 9, 46, 9, 25, 33, 4, 30, 27, 50];
-
-const hoverBackgroundColor = 'linear-gradient(180deg, #34CAA5 0%, rgba(52, 202, 165, 0.00) 100%)'
     
  const options: ChartOptions<'bar'> = {
       responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
       scales: {
         x: {
           grid: {
             display: false,
           },
-
         },
         y: {
           grid: {
             display: true,
-            drawTicks: false,
           },
           border: {
             dash: [3, 5],
             display: false,
-            dashOffset: 100
-          }, 
-          ticks: {
-            
+        }, 
+        ticks: {
+          maxTicksLimit: 10,
+          callback: (value) => {
+            if (value === 0) {
+              return 0;
+            }
+        
+            const numericValue = typeof value === 'string' ? parseFloat(value) : value;
+        
+            return `${numericValue.toFixed(3)}`; // Format the tick labels to display '.000'
           },
-
-        }
+        },
+        
+        },
       },
     };
     
@@ -65,10 +75,27 @@ const hoverBackgroundColor = 'linear-gradient(180deg, #34CAA5 0%, rgba(52, 202, 
             borderRadius: 360,
             borderWidth: 1,
             hoverBackgroundColor: 'rgba(52, 202, 165, .5)',
-            data: salesData.map(number => Number(number.toFixed(3))),
+            data: salesData
+            ,
           },
         ],
       };
   
-  return <Bar options={options} data={data} />;
+  return (
+    <div className='flex flex-col bg-white p-2 rounded-xl'>
+
+      <div className='flex flex-row justify-between items-center mb-5'>
+        <h5 className=' text-lg'>Sales Trend</h5>
+        <div className='flex'>
+          <span className='mr-2'>Sort by:</span>
+          <div className='flex border border-[##E1DFDF] rounded-full text-md px-2 items-center'>
+            <span>Weekly</span>
+            <img src={ArrowDown} className=' ml-1 w-4 h-4'/>
+          </div>
+        </div>
+      </div>
+
+      <Bar options={options} data={data} />
+    </div>
+  );
 }
