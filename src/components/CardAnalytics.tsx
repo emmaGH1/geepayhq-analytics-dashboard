6fc4fa135dfb16917488e660b6c7094c5acf1afd
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-26 04:45:42 +0100

    feat: Setup card analytics

---
+++ src/components/CardAnalytics.tsx
@@ -0,0 +1,45 @@
+import { BoxCart, BoxCoin, BoxRotate, BoxTick, DownTrend, DownTrendArrow, UpTrend, UpTrendArrow } from "../constants";
+import { Card } from "./Card";
+
+const CardAnalytics = () => {
+  return (
+    <div className="grid lg:grid-cols-2 w-2/4 gap-4">
+        <Card 
+          BoxIcon={BoxTick} 
+          Name='Total Order'
+          Amount={350}
+          TrendArrowIcon={UpTrendArrow}
+          Percentage={23.5}
+          TrendIcon={UpTrend}
+        />
+        <Card 
+          isTrendUp={false}
+          BoxIcon={BoxRotate} 
+          TrendIcon={DownTrend}
+          Name='Total Refund'
+          Amount={270}
+          TrendArrowIcon={DownTrendArrow}
+          Percentage={7}
+        />
+        <Card 
+          BoxIcon={BoxCart} 
+          TrendIcon={DownTrend}
+          Name='Average Sales'
+          Amount={1567}
+          TrendArrowIcon={DownTrendArrow}
+          Percentage={13}
+          isTrendUp={false}
+        />
+        <Card 
+          BoxIcon={BoxCoin} 
+          TrendIcon={UpTrend}
+          Name='Total Order'
+          Amount={350}
+          TrendArrowIcon={UpTrendArrow}
+          Percentage={57}
+        />
+    </div>
+  );
+};
+
+export default CardAnalytics;
