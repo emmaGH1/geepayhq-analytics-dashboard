b3e2e8d5ed229c92b3644ed1c94757e54ea1632a
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-27 04:44:24 +0100

    feat: Add sonner to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import MobileNav from "./components/ui/MobileNav";
 import MobileNavContent from "./components/ui/MobileNavContent";
 import { Sidenav } from "./components/ui/Sidenav";
 import { Topbar } from "./components/ui/Topbar";
-
+import { Toaster } from '@/components/ui/sonner'
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className="bg-background text-secondary flex flex-row w-full relative">
        <Sidenav />
        <div className="flex flex-col w-full relative">
-          <Topbar />
-          <MobileNav />
-          <MobileNavContent />
+          <Topbar/>
+          <MobileNav setIsOpen={setIsOpen} />
+          {isOpen && !!isOpen && <MobileNavContent setIsOpen={setIsOpen} />}
           <Dashboard />
+          <Toaster className="text-black"/>
        </div>
     </div>
   );
